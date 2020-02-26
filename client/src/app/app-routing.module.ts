70de78d0651c6e6fec1b5e87c22dd82d5b65f319
Mac <mac@MacBook-Pro-de-Mac.local>
2020-02-26 14:13:22 -0600

    Modulos creados

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
+  { path: 'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
