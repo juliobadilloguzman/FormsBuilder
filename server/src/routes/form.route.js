69dbe75454d2618cce6dcd42f3b8c0d422b79090
Diego-PC <diego.mmtz@hotmail.com>
2020-03-07 02:02:33 -0600

    En un solo POST request ya se crea un cuestionario y sus preguntas

--- server/src/routes/form.route.js
+++ server/src/routes/form.route.js
@@ -6,8 +6,6 @@ let formController = require('../controllers/form.controller');
 router.post('/createform', formController.CreateUpdateForm);
 router.post('/getForms', formController.getFormByUserId);
 router.get('/:idCuestionario', formController.getFormById);
-router.post('/createOpenQuestion', formController.createOpenQuestion);
-
 
 //**** MIDDLEWARE para proteger las rutas ****/
 // router.use((req, res, next) => {
